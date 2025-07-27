606fc139daa104638d2871ffb07e34152bd09657
bindudokala <158530964+bindudokala@users.noreply.github.com>
2025-07-27 18:32:04 -0400

    Time: 19 ms (67.63%), Space: 18.8 MB (23.99%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,14 +1,10 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        max_reachable = 0
-        for current in range(len(nums)):
-            if current > max_reachable:
-                return False
-            max_reachable = max(max_reachable, current + nums[current])
-            if max_reachable >= len(nums)-1:
-                return True
-        return True
-            
-            
-
-        
+        # start from the end of the array, for every index check whether we can reach the goal from the particular index. if yes change the goal to current index.
+        goal = len(nums) - 1 
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] + i >= goal:
+                goal = i
+        return True if goal == 0 else False
+ 
+ # Test case [2, 0, 0]
