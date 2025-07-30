75c74c631907bc6d4fa6910720eff1bf6e1d25e3
bindudokala <158530964+bindudokala@users.noreply.github.com>
2025-07-30 02:49:45 -0400

    Time: 55 ms (67.24%), Space: 32.9 MB (22.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        # Kadane's Algorithm - whenever the current sum becomes negative, reset it to 0.
         max_sum, curr_sum = nums[0], 0
         for i in range(len(nums)):
             if curr_sum < 0:
