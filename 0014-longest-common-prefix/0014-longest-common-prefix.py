4ea2d75bf0af54169bda1d52651eebf8ee46de08
bindudokala <158530964+bindudokala@users.noreply.github.com>
2025-11-12 01:54:07 -0500

    Time: 3 ms (26.34%), Space: 17.7 MB (78.31%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,5 +1,7 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        '''Meth1 : Sort the array of strings. Compare the first and last strings for the result.     
+        Meth2: Find the smallest string length in the strs array and run a loop of the smallest string length through the array and start comparing all the string at the index.'''
         commonPrefix = ""
         minLength = float('inf')
         for s in strs:
@@ -9,4 +11,4 @@ class Solution:
             for s in strs:
                 if s[i] != strs[0][i]:
                     return s[:i]
-        return strs[:i]    
+        return strs[0][:minLength]    
