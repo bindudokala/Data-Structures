23077608a4f390a619dba4dbe15404e986577041
bindudokala <158530964+bindudokala@users.noreply.github.com>
2025-11-12 00:20:45 -0500

    Time: 6 ms (46.8%), Space: 18 MB (20.3%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,5 +1,7 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        '''Meth 1:  I did take IV, XL, XC, CD, CM to the hashmap so while looping checked for s[i] + s[I + 1] in hashmap… 
+        We can also do Meth2: Only take the given Roman values in hashmap and when s[i] < s[i+1] subtract the s[i] value from integer formed.'''
         integer = 0
         i = 0
         romanToInt = {'I' : 1, 'IV' : 4, 'V': 5, 'IX' : 9, 'X' : 10, 'XL' : 40, 'L' : 50, 'XC' : 90, 'C' : 100, 'CD' : 400, 'D' : 500, 'CM' : 900, 'M' : 1000}
