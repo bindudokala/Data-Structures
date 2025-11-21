af86b2430128ade6b4039d1ff287a1f7f7403430
bindudokala <158530964+bindudokala@users.noreply.github.com>
2025-11-21 02:55:47 -0500

    Time: 0 ms (100%), Space: 17.6 MB (82.34%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.py
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -1,18 +1,18 @@
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        # # Using Dictionary
-        # jewels_dict = defaultdict(int)
-        # for ch in jewels:
-        #     jewels_dict[ch] = 0
+        # Using Dictionary
+        jewels_dict = defaultdict(int)
+        for ch in jewels:
+            jewels_dict[ch] = 0
         
-        # for ch in stones:
-        #     if ch in jewels_dict:
-        #         jewels_dict[ch] += 1
+        for ch in stones:
+            if ch in jewels_dict:
+                jewels_dict[ch] += 1
         
-        # result = 0
-        # for k in jewels_dict:
-        #     result += jewels_dict[k]
-        # return result
+        result = 0
+        for k in jewels_dict:
+            result += jewels_dict[k]
+        return result
 
         # Using Hashset
         jewels_set = set(jewels)
